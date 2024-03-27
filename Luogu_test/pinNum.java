e54198104e9c48e91b82e9d1e0dbc819a7fcf921
Cunninger <1803912219@qq.com>
2024-03-27 14:12:54 +0800

    BleuBridge pinNum

---
+++ Luogu_test/pinNum.java
@@ -0,0 +1,81 @@
+package Luogu_test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class pinNum {
+
+    public static void main(String[] args) {
+        QuickInput in = new QuickInput();
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.next();
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String a, String b) {
+                return -((a + b).compareTo(b + a));
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+        }
+        out.println(sb.toString());
+
+        out.close();
+    }
+}
+
+class QuickInput {
+    BufferedReader buf;
+    StringTokenizer tok;
+
+    QuickInput() {
+        buf = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    boolean hasNext() {
+        while (tok == null || !tok.hasMoreElements()) {
+            try {
+                tok = new StringTokenizer(buf.readLine());
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    String next() {
+        if (hasNext())
+            return tok.nextToken();
+        return null;
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    BigInteger nextBigInteger() {
+        return new BigInteger(next());
+    }
+
+    BigDecimal nextBigDecimal() {
+        return new BigDecimal(next());
+    }
+}
--- Quickgit.bat
+++ Quickgit.bat
@@ -7,7 +7,7 @@ REM 添加所有改动到暂存区
 git add .
 
 REM 提交改动
-git commit -m "BleuBridge reverseString "
+git commit -m "BleuBridge pinNum "
 
 REM 推送改动到远程仓库
 git push origin master
