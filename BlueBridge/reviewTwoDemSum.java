319d881f288f55a5d88168c38fff458ee8554cea
Cunninger <1803912219@qq.com>
2024-03-30 17:24:46 +0800

    BleuBridge reviewTwoDemSum

---
+++ BlueBridge/reviewTwoDemSum.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+import codeforce.practice_solve.one;
+
+public class reviewTwoDemSum {
+    static int[][] ssm;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = n;
+        int[][] arr = new int[n + 1][m + 1];
+        ssm = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = in.nextInt();
+
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                ssm[i][j] = ssm[i - 1][j] + ssm[i][j - 1] - ssm[i - 1][j - 1] + arr[i][j];
+
+            }
+        }
+        int ma = Integer.MIN_VALUE;
+        for (int x1 = 1; x1 < arr.length; x1++) { // x1
+            for (int y1 = 1; y1 < arr.length; y1++) { // x2
+                for (int x2 = x1; x2 < arr.length; x2++) { // y1
+                    for (int y2 = y1; y2 < arr.length; y2++) { // y2
+
+                        ma = Math.max(ma, getPart(x1, y1, x2, y2));
+                    }
+                }
+            }
+        }
+        System.out.println(ma);
+
+    }
+
+    public static int getPart(int x1, int y1, int x2, int y2) {
+        // a++;
+        // b++;
+        // c++;
+        // d++;
+        return ssm[x2][y2] - ssm[x2][y1 - 1] - ssm[x1 - 1][y2] + ssm[x1 - 1][y1 - 1];
+    }   
+}
--- Quickgit.bat
+++ Quickgit.bat
@@ -7,7 +7,7 @@ REM 添加所有改动到暂存区
 git add .
 
 REM 提交改动
-git commit -m "BleuBridge chessPan "
+git commit -m "BleuBridge reviewTwoDemSum "
 
 REM 推送改动到远程仓库
 git push origin master
