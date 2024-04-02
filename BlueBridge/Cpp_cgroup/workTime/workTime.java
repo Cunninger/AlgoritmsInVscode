459908dc9e4eaa7d199e9ca10ceae3371bb3edaf
Cunninger <1803912219@qq.com>
2024-04-02 14:37:38 +0800

    blueBridge workTime finsh

--- BlueBridge/Cpp_cgroup/workTime/workTime.java
+++ BlueBridge/Cpp_cgroup/workTime/workTime.java
@@ -9,20 +9,34 @@ public class workTime {
         String[] arr = new String[260];
         Scanner in = new Scanner(file);
         int k = 0;
-        while (in.hasNextLine() && k < 520) {
-            if (k != 0 && k % 2 == 0) {
-
-                arr[k] = in.nextLine();
-                
+        int hang = 1;
+        while (in.hasNextLine()&& hang<521) {
+            String s = in.nextLine();
+            if (hang % 2 == 0) {
+                arr[k++] = s;         
             }
-            k++;
+            hang++;
+
         
         }
 
-        for (int i = 0; i < 260; i++) {
-            System.out.println(arr[i]);
-        }
+        // for (int i = 0; i < 260; i++) {
+        //     System.out.println(arr[i]);
+        // }
         // hello world!!!
+
+            int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            String[] ss = arr[i].split(":");
+            int hour = Integer.valueOf(ss[0]);
+            int min = Integer.valueOf(ss[1]);
+            int sec = Integer.valueOf(ss[2]);
+
+            sum+=hour*3600+min*60+sec;
+
+
+        }
+        System.out.println(sum);
 
     }
 
