4b849b3e0b4da50572c393137297017fb056d06b
Cunninger <1803912219@qq.com>
2024-04-04 16:58:40 +0800

    blueBridge  thirtheen 13 B makeProbemCal fixed

--- BlueBridge/thirtheen_Bgroup/makeProbemCal.java
+++ BlueBridge/thirtheen_Bgroup/makeProbemCal.java
@@ -6,15 +6,22 @@ public class makeProbemCal {
     public static void main(String[] args) {
      
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int n = in.nextInt();
-        int []week = new int[]{a,a,a,a,a,b,b};
-        int k = 0;
-        while (n>0){
-            n-=week[(k++)%7];
+        long a = in.nextLong();
+        long b = in.nextLong();
+        long n = in.nextLong();
+        long unit = 5*a+2*b;
+        long neWeek = n/unit;
+        long rest = n%unit;
+        long day = neWeek*7;
+        int x= 1;
+        while (rest>0) {
+            if (x%7==6||x%7==0)rest-=b;
+            else rest-=a;
+            x++;
+            day++;
         }
-        System.out.println(k);
+
+        System.out.println(day);
 
     }
 }
